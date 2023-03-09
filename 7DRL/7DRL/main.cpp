9b4861f57049b1fcd7af174d79aae5df21576f8d
Fernando <fernandoairescastello@gmail.com>
2023-03-09 20:40:45 -0300

    View history

--- 7DRL/7DRL/main.cpp
+++ 7DRL/7DRL/main.cpp
@@ -13,7 +13,7 @@ enum class t_direction {
 };
 
 enum class t_terrain {
-	oob, ground, wall, stairs
+	oob, ground, wall, stairs, coin
 };
 
 enum class t_entity {
@@ -33,8 +33,8 @@ struct t_exit {
 };
 
 struct t_floor {
-	static const int width = 102;
-	static const int height = 102;
+	static const int width = 100;
+	static const int height = 100;
 	t_location oob = t_location();
 	rgb forecolor = 0xffffff;
 	rgb backcolor = 0x000000;
@@ -128,6 +128,7 @@ struct t_bomb {
 			y = by;
 			timer = 0;
 			active = true;
+			tgl.play_notes("l30o4cde");
 		}
 	}
 	void tick()
@@ -143,6 +144,7 @@ struct t_bomb {
 	{
 		timer = 0;
 		active = false;
+		tgl.sound("explosion");
 
 		cur_floor.detonate_wall(x - 1, y - 1);
 		cur_floor.detonate_wall(x + 0, y - 1);
@@ -156,21 +158,21 @@ struct t_bomb {
 	}
 };
 
+void trigger_player_collisions();
 struct t_player {
 	t_direction dir = t_direction::none;
 	t_bomb bomb;
 
+	int get_floor() { return floor_nr; }
 	int get_x() { return x; }
 	int get_y() { return y; }
 	int get_prevx() { return prevx; }
 	int get_prevy() { return prevy; }
 	int get_life() { return life; }
+	int get_coins() { return coins; }
+	int get_bombs() { return bombs; }
 
-	bool oob()
-	{
-		return x < 0 || y < 0 || x >= cur_floor.width || y >= cur_floor.height;
-	}
-	void setpos(int newx, int newy)
+	void set_pos(int newx, int newy)
 	{
 		prevx = x;
 		prevy = y;
@@ -189,8 +191,9 @@ struct t_player {
 		int newx = x + dx;
 		int newy = y + dy;
 		if (can_move_to(newx, newy)) {
-			setpos(newx, newy);
+			set_pos(newx, newy);
 			view.scroll(dx, dy);
+			trigger_player_collisions();
 		}
 	}
 	bool can_move_to(int newx, int newy)
@@ -199,7 +202,7 @@ struct t_player {
 		t_terrain& t = loc.terrain;
 		t_entity& e = loc.entity;
 		
-		return t == t_terrain::ground || t == t_terrain::stairs;
+		return t == t_terrain::ground || t == t_terrain::stairs || t == t_terrain::coin;
 	}
 	t_location& getloc()
 	{
@@ -209,11 +212,38 @@ struct t_player {
 	{
 		return getloc().terrain == t_terrain::stairs;
 	}
+	bool found_coin()
+	{
+		return getloc().terrain == t_terrain::coin;
+	}
+	void grab_coin()
+	{
+		coins++;
+		tgl.sound("coin");
+		getloc().terrain = t_terrain::ground;
+	}
 	void drop_bomb()
 	{
-		if (!bomb.active) {
+		if (!bomb.active && bombs > 0) {
+			bombs--;
 			bomb.activate(x, y);
 		}
+	}
+	void destroy_walls_around()
+	{
+		cur_floor.set(t_terrain::ground, x - 1, y - 1);
+		cur_floor.set(t_terrain::ground, x + 0, y - 1);
+		cur_floor.set(t_terrain::ground, x + 1, y - 1);
+		cur_floor.set(t_terrain::ground, x - 1, y);
+		cur_floor.set(t_terrain::ground, x, y);
+		cur_floor.set(t_terrain::ground, x + 1, y);
+		cur_floor.set(t_terrain::ground, x - 1, y + 1);
+		cur_floor.set(t_terrain::ground, x + 0, y + 1);
+		cur_floor.set(t_terrain::ground, x + 1, y + 1);
+	}
+	void next_floor()
+	{
+		floor_nr++;
 	}
 private:
 	int x = 0;
@@ -221,6 +251,9 @@ private:
 	int prevx = x;
 	int prevy = y;
 	int life = 100;
+	int coins = 0;
+	int bombs = 10;
+	int floor_nr = 0;
 };
 t_player player;
 
@@ -240,6 +273,15 @@ void generate_room(int x, int y, int w, int h)
 	for (int py = y; py <= y + h; py++) {
 		cur_floor.set(t_terrain::wall, x, py);
 		cur_floor.set(t_terrain::wall, x + w, py);
+	}
+	// coins
+	for (int py = y + 1; py <= y + h - 1; py++) {
+		for (int px = x + 1; px <= x + w - 1; px++) {
+			bool chance = tgl.rnd(0, 100) > 50;
+			if (chance) {
+				cur_floor.set(t_terrain::coin, px, py);
+			}
+		}
 	}
 
 	// room entrance & room exit
@@ -266,7 +308,9 @@ void generate_wall(int x, int y, int length, int orient)
 	int px = x;
 	int py = y;
 	for (int i = 0; i <= length; i++) {
-		cur_floor.set(t_terrain::wall, px, py);
+		if (cur_floor.get(px, py).entity == t_entity::none) {
+			cur_floor.set(t_terrain::wall, px, py);
+		}
 		if (orient == 0) {
 			px++;
 		} else {
@@ -276,31 +320,96 @@ void generate_wall(int x, int y, int length, int orient)
 }
 
 struct {
-	vector<rgb> background = {
-		0x101010, // black,
-		0xc0c0c0, // gray,
-		0x202050, // indigo
-		0x3030c0, // blue,
-		0x802020, // red
-	};
-	vector<rgb> foreground = {
-		0x3050ff, // blue
-		0x30ffff, // cyan
-		0x30ff50, // green
-		0xff4080, // pink
-		0xff4040, // red
-		0xffa030, // orange
-		0xffff50, // yellow
-		0xe0e0e0, // white
-		0x404040, // gray
+	vector<pair<rgb, rgb>> presets = {
+		// purple backgrounds
+		{ 0x1010a0, 0x902080 }, // blue-purple
+		{ 0xe05080, 0x902080 }, // pink-purple
+		{ 0xe0e040, 0x902080 }, // yellow-purple
+		{ 0x10e010, 0x902080 }, // green-purple
+		{ 0x10c0ff, 0x902080 }, // cyan-purple
+		{ 0xe0e0e0, 0x902080 }, // white-purple
+		{ 0x101010, 0x902080 }, // black-purple
+		// amber backgrounds
+		{ 0x101010, 0xffa030 }, // black-amber
+		{ 0xe02020, 0xe09020 }, // red-amber
+		{ 0xe0e0e0, 0xe09020 }, // white-amber
+		// pink backgrounds
+		{ 0x10d010, 0xe04080 }, // green-pink
+		{ 0x10c0ff, 0xe04080 }, // cyan-pink
+		{ 0xe0e040, 0xe04080 }, // yellow-pink
+		{ 0x2020d0, 0xe04080 }, // blue-pink
+		{ 0xe0e0e0, 0xe04080 }, // white-pink
+		{ 0xc01010, 0xe04080 }, // red-pink
+		{ 0x101010, 0xe04080 }, // black-pink
+		// gray backgrounds
+		{ 0xc0c0c0, 0x606060 }, // white-gray
+		{ 0xe05080, 0x606060 }, // pink-gray
+		{ 0x1010a0, 0x606060 }, // blue-gray
+		{ 0x10a010, 0x606060 }, // green-gray
+		{ 0x101010, 0x606060 }, // black-gray
+		{ 0xa02020, 0x606060 }, // red-gray
+		// red backgrounds
+		{ 0xe01010, 0x901010 }, // red-red
+		{ 0x10c0ff, 0xa02020 }, // cyan-red
+		{ 0xffa030, 0xa02020 }, // amber-red
+		{ 0xe0e040, 0xa02020 }, // yellow-red
+		{ 0xe0e0e0, 0xa02020 }, // white-red
+		{ 0x101010, 0xa02020 }, // black-red
+		// green backgrounds
+		{ 0x904020, 0x20a020 }, // brown-green
+		{ 0x30ffff, 0x20a020 }, // cyan-green
+		{ 0x2020a0, 0x20a020 }, // blue-green
+		{ 0xa02020, 0x20a020 }, // red-green
+		{ 0xb02090, 0x20a020 }, // purple-green
+		{ 0xe0e0e0, 0x20a020 }, // white-green
+		{ 0x101010, 0x20a020 }, // black-green
+		// cyan backgrounds
+		{ 0x20ffff, 0x20d0d0 }, // cyan-cyan
+		{ 0xffffff, 0x20d0d0 }, // white-cyan
+		{ 0x20a020, 0x20e0e0 }, // green-cyan
+		{ 0x2020a0, 0x20e0e0 }, // blue-cyan
+		{ 0xe02020, 0x20e0e0 }, // red-cyan
+		{ 0x101010, 0x20e0e0 }, // black-cyan
+		// yellow backgrounds
+		{ 0xc030a0, 0xe0e040 }, // purple-yellow
+		{ 0x2020a0, 0xe0e040 }, // blue-yellow
+		{ 0x20a020, 0xe0e040 }, // green-yellow
+		{ 0xe02020, 0xe0e040 }, // red-yellow
+		{ 0x101010, 0xe0e040 }, // black-yellow
+		// blue backgrounds
+		{ 0xff4080, 0x3030c0 }, // pink-blue
+		{ 0x101010, 0x3030c0 }, // black-blue
+		{ 0x3050ff, 0x3030c0 }, // blue-blue
+		{ 0x30ffff, 0x3030c0 }, // cyan-blue
+		{ 0xff6060, 0x3030c0 }, // red-blue		
+		{ 0x30ff50, 0x3030c0 }, // green-blue
+		{ 0xffff50, 0x3030c0 }, // yellow-blue
+		{ 0xe0e0e0, 0x3030c0 }, // white-blue
+		// white backgrounds
+		{ 0xffffff, 0xe0e0e0 }, // white-white
+		{ 0x10c0c0, 0xe0e0e0 }, // cyan-white
+		{ 0x1030e0, 0xe0e0e0 }, // blue-white
+		{ 0xe02020, 0xe0e0e0 }, // red-white
+		{ 0x108020, 0xe0e0e0 }, // green-white
+		{ 0x101010, 0xe0e0e0 }, // black-white
+		// black backgrounds
+		{ 0xc030a0, 0x101010 }, // purple-black
+		{ 0xff4080, 0x101010 }, // pink-black
+		{ 0xe0e0e0, 0x101010 }, // white-black
+		{ 0x404040, 0x101010 }, // gray-black
+		{ 0xffff50, 0x101010 }, // yellow-black
+		{ 0xffa030, 0x101010 }, // amber-black
+		{ 0x3050ff, 0x101010 }, // blue-black
+		{ 0x30ffff, 0x101010 }, // cyan-black
+		{ 0x30ff50, 0x101010 }, // green-black
+		{ 0xff4040, 0x101010 }, // red-black
 	};
 } color_scheme;
 
 void randomize_color_scheme()
 {
-	int fgc_index = tgl.rnd(0, color_scheme.foreground.size() - 1);
-	int bgc_index = tgl.rnd(0, color_scheme.background.size() - 1);
-	cur_floor.color(color_scheme.foreground[fgc_index], color_scheme.background[bgc_index]);
+	int color_preset = tgl.rnd(0, color_scheme.presets.size() - 1);
+	cur_floor.color(color_scheme.presets[color_preset].first, color_scheme.presets[color_preset].second);
 }
 void init_current_floor()
 {
@@ -333,14 +442,6 @@ void init_current_floor()
 		cur_floor.set(t_terrain::wall, 0, y);
 		cur_floor.set(t_terrain::wall, cur_floor.width - 1, y);
 	}
-	// rooms
-	for (int i = 0; i < room_count; i++) {
-		int x = tgl.rnd(0, cur_floor.width);
-		int y = tgl.rnd(0, cur_floor.height);
-		int w = tgl.rnd(min_room_w, max_room_w);
-		int h = tgl.rnd(min_room_h, max_room_h);
-		generate_room(x, y, w, h);
-	}
 	// random walls
 	for (int i = 0; i < random_wall_count; i++) {
 		int x = tgl.rnd(0, cur_floor.width);
@@ -348,6 +449,14 @@ void init_current_floor()
 		int length = tgl.rnd(2, max_wall_len);
 		int orient = tgl.rnd(0, 1);
 		generate_wall(x, y, length, orient);
+	}
+	// rooms
+	for (int i = 0; i < room_count; i++) {
+		int x = tgl.rnd(0, cur_floor.width);
+		int y = tgl.rnd(0, cur_floor.height);
+		int w = tgl.rnd(min_room_w, max_room_w);
+		int h = tgl.rnd(min_room_h, max_room_h);
+		generate_room(x, y, w, h);
 	}
 	// exits
 	for (int i = 0; i < exit_count; i++) {
@@ -378,7 +487,9 @@ void draw_current_floor()
 					}
 					// terrain
 					else {
-						if (loc.terrain == t_terrain::ground) {
+						if (loc.terrain == t_terrain::coin) {
+							tgl.draw_tiled("collect", scrx, scry);
+						} else if (loc.terrain == t_terrain::ground) {
 							if (loc.terrain_variant == 0) {
 								tgl.draw_tiled("ground", scrx, scry);
 							} else if (loc.terrain_variant == 1) {
@@ -410,8 +521,15 @@ void draw_info()
 {
 	tgl.font_transparent(false);
 	tgl.font_color(cur_floor.forecolor, cur_floor.backcolor);
-	tgl.print_tiled(tgl.fmt("E%iN%i", player.get_x(), player.get_y()), 1, 0);
-	tgl.print_tiled(tgl.fmt("~%03i", player.get_life()), 1, screen.rows - 1);
+	// top
+	int y = 0;
+	tgl.print_tiled(tgl.fmt("F%i E%iN%i", player.get_floor(), player.get_x(), player.get_y()), 1, y);
+	// bottom
+	y = screen.rows - 1;
+	tgl.print_tiled(tgl.fmt("~%03i  %02i %8i", 
+		player.get_life(), player.get_bombs(), player.get_coins()), 1, y);
+	tgl.draw_tiled("bomb", 6, y);
+	tgl.draw_tiled("collect", 18, y);
 }
 void sync_player()
 {
@@ -433,7 +551,11 @@ void visit_surroundings()
 	cur_floor.visit(x + 0, y + 1);
 	cur_floor.visit(x + 1, y + 1);
 }
-
+void init_sounds()
+{
+	tgl.sound_load("explosion", "sound/bomb.wav");
+	tgl.sound_load("coin", "sound/coin.wav");
+}
 void init_tiles();
 void goto_next_floor();
 void game_init()
@@ -441,20 +563,74 @@ void game_init()
 	tgl.window_gbc(0x000000, 5);
 	screen.cols = tgl.cols();
 	screen.rows = tgl.rows();
+	tgl.mouse(false);
 
 	init_tiles();
+	init_sounds();
 	goto_next_floor();
+}
+void draw_floor_intro()
+{
+	tgl.play_notes("l40o5cdedef");
+	tgl.backcolor(cur_floor.backcolor);
+
+	int timer = 300;
+	while (tgl.running() && timer > 0) {
+		tgl.clear();
+		tgl.font_transparent(false);
+		tgl.font_color(cur_floor.forecolor, cur_floor.backcolor);
+		tgl.print_tiled(tgl.fmt("FLOOR %4i", player.get_floor()), 5, 9);
+		tgl.system();
+		timer--;
+	}
 }
 void goto_next_floor()
 {
+	player.next_floor();
+
+	int player_x = tgl.rnd(1, cur_floor.width - 1);
+	int player_y = tgl.rnd(1, cur_floor.height - 1);
+	player.set_pos(player_x, player_y);
+	
 	init_current_floor();
-	player.setpos(tgl.rnd(1, cur_floor.width - 1), tgl.rnd(1, cur_floor.height - 1));
-	view.setscrl(player.get_x() - screen.cols / 2 + 1, player.get_y() - screen.rows / 2 + 1);
+	cur_floor.set(t_terrain::ground, player_x, player_y);
+	player.destroy_walls_around();
+
+	view.setscrl(player_x - screen.cols / 2 + 1, player_y - screen.rows / 2 + 1);
+
+	draw_floor_intro();
 }
-void check_collisions()
+void confirm_goto_next_floor()
+{
+	tgl.print_tiled("   Descend? (Y/N)   ", 0, screen.rows - 1);
+
+	bool finished = false;
+	bool confirmed = false;
+
+	int key = 0;
+
+	while (tgl.running() && !finished) {
+		tgl.system();
+		key = tgl.kb_lastkey();
+		if (key == SDLK_y) {
+			confirmed = true;
+			finished = true;
+		} else if (key == SDLK_n || key == SDLK_ESCAPE) {
+			confirmed = false;
+			finished = true;
+		}
+	}
+	
+	if (confirmed) {
+		goto_next_floor();
+	}
+}
+void trigger_player_collisions()
 {
 	if (player.is_on_stairs()) {
-		goto_next_floor();
+		confirm_goto_next_floor();
+	} else if (player.found_coin()) {
+		player.grab_coin();
 	}
 }
 void tick_bomb_timers()
@@ -472,18 +648,17 @@ void game_loop()
 	draw_info();
 	tgl.system();
 
-	check_collisions();
 	tick_bomb_timers();
 
 	int key = tgl.kb_lastkey();
-	if (tgl.kb_esc()) tgl.exit();
-
-	if (key == SDLK_RIGHT) player.move(1, 0);
+	if (key == SDLK_ESCAPE) tgl.exit();
+	else if (key == SDLK_RIGHT) player.move(1, 0);
 	else if (key == SDLK_LEFT) player.move(-1, 0);
 	else if (key == SDLK_DOWN) player.move(0, 1);
 	else if (key == SDLK_UP) player.move(0, -1);
 	else if (key == SDLK_c) randomize_color_scheme();
 	else if (key == SDLK_SPACE) player.drop_bomb();
+	else if (key == SDLK_TAB) confirm_goto_next_floor();
 }
 int main(int argc, char* argv[])
 {
@@ -578,17 +753,28 @@ void init_tiles()
 	);
 	tgl.tile_add("solid", "solid");
 
-	tgl.tile_new("marker",
-		"10000001"
-		"01000010"
-		"00100100"
-		"00011000"
-		"00011000"
-		"00100100"
-		"01000010"
-		"10000001"
+	tgl.tile_new("collect_1",
+		"00111000"
+		"01000100"
+		"10111010"
+		"10111010"
+		"10111010"
+		"01000100"
+		"00111000"
+		"00000000"
 	);
-	tgl.tile_add("marker", "marker");
+	tgl.tile_new("collect_2",
+		"00111000"
+		"01101100"
+		"01010100"
+		"01010100"
+		"01010100"
+		"01101100"
+		"00111000"
+		"00000000"
+	);
+	tgl.tile_add("collect", "collect_1");
+	tgl.tile_add("collect", "collect_2");
 
 	tgl.tile_new("wall",
 		"11111011"
